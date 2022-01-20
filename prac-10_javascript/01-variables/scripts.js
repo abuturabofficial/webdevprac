0dffafec21658dd0f70b98eaad5bf513dde0aa86
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-01-20 15:22:20 +0500

    add prac for JS variables

--- prac-10_javascript/01-variables/scripts.js
+++ prac-10_javascript/01-variables/scripts.js
@@ -1,5 +1,47 @@
-let message;
-message = "hello!";
-alert(message)
+// let message
+// message = 'hello!, World'
+// console.log(message)
+// console.log('hello, world! I am Sajid')
 
-console.log("hello, world!")
+//We can declare and assign value to the variables on a single line
+// let message = 'Hello!';
+// alert(message)
+
+// we can declare multiple variables on single line
+// let user = 'John', age = '22', message = 'Hello!';
+// console.log(user,age,message)
+// alert(user)
+// alert(age)
+// alert(message) // Using single line variables is not recommedded
+
+// Two other ways of declaring variables
+// let user = 'John',
+//   age = 25,
+//   message = 'Hello!';
+
+// Or
+// let user = 'John'
+//   , age = '25'
+//   , message = 'Hello!';
+
+// var can also declare variables in old school way
+// var message = 'hello!';
+// console.log(message)
+
+// same variables can be declare multiple times but old value will be deleted
+// let message;
+// message = 'Hello!';
+// message = 'World'; //World will be saved and hello will be deleted
+// console.log(message)
+
+// We can also declare two variables and copy data from one another
+// let hello = 'hello!, world'
+// let message
+// message = hello
+// alert (hello)
+// alert(message)
+
+// Declaring a same variable twice throws Syntax Error
+// let message = 'This'
+// let message = 'that'
+
