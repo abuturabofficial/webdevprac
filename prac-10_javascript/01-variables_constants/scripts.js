db00885f2bb4cfbcf3bd28501cada1ec7f29a8c2
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-01-28 11:23:01 +0500

    add prac for JS variables and constants

---
+++ prac-10_javascript/01-variables_constants/scripts.js
@@ -0,0 +1,77 @@
+// let message
+// message = 'hello!, World'
+// console.log(message)
+// console.log('hello, world! I am Sajid')
+
+//We can declare and assign value to the variables on a single line
+// let message = 'Hello!';
+// alert(message)
+
+// we can declare multiple variables on single line
+// let user = 'John', age = '22', message = 'Hello!';
+// console.log(user,age,message)
+// alert(user)
+// alert(age)
+// alert(message) // Using single line variables is not recommended
+
+// Two other ways of declaring variables
+// let user = 'John',
+//   age = 25,
+//   message = 'Hello!';
+
+// Or
+// let user = 'John'
+//   , age = '25'
+//   , message = 'Hello!';
+
+// var can also declare variables in old school way
+// var message = 'hello!';
+// console.log(message)
+
+// same variables can be declare multiple times but old value will be deleted
+// let message;
+// message = 'Hello!';
+// message = 'World'; //World will be saved and hello will be deleted
+// console.log(message)
+
+// We can also declare two variables and copy data from one another
+// let hello = 'hello!, world'
+// let message
+// message = hello
+// alert (hello)
+// alert(message)
+
+// Declaring a same variable twice throws Syntax Error
+// let message = 'This'
+// let message = 'that'
+
+// $ and _ are valid names for variables
+// let $ = 3;
+// let _ = 4;
+// alert($ + _);
+
+// an assignment without 'use strict'
+// num = 5;
+// alert(num);
+
+// "use strict";
+// num = 5;
+// alert(num);
+
+// CONSTANTS
+
+// const myBirthday = "24.03.1990";
+// alert(myBirthday);
+//
+
+// TASKS
+
+// let admin;
+// let nameOfPerson;
+// nameOfPerson = "John";
+// admin = nameOfPerson;
+// alert(nameOfPerson);
+// alert(admin);
+
+// let ourPlanetName= 'earth';
+// let currentVisitorName = 'John';
