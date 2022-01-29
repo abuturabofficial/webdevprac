b7f1a0c6e598acce0954e2ef3fd221eaba871980
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-01-29 14:26:27 +0500

    add prac for JS arithmetics and Numbers

---
+++ prac-10_javascript/02-js_arithmetics/scripts.js
@@ -0,0 +1,94 @@
+// Arithmetic Operations
+//
+// Adding
+//
+// let x = 100 + 50;
+// alert(x);
+
+// let x = a + b;
+
+// let x = (100 + 50) * a;
+
+//Multiplication
+//
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// alert(z);
+
+// Dividing
+//
+// let x = 5;
+// let y = 2;
+// let z = x / y;
+// alert(z);
+
+// Remainder
+//
+// let x = 5;
+// let y = 2;
+// let z = x % y;
+// alert(z);
+
+// Incrementing
+//
+// let x = 5;
+// x++;
+// let z = x;
+// alert(z);
+
+// Decrementing
+//
+// let x = 5;
+// x--;
+// let z = x;
+// alert(z);
+
+// Exponentiation
+//
+// let x = 5;
+// let z = x ** 2;
+// alert(z);
+//
+// let x = 5;
+// let z = Math.pow(x, 2);
+// alert(z);
+
+// let x = 10,
+//   y = 5;
+// x *= y;
+// alert(x);
+
+// let x = 0.2 + 0.1;
+// alert(x);
+
+// let x = (0.2 * 10 + 0.1 * 10) / 10;
+// alert(x);
+
+// Adding Numbers and Strings
+//
+// let x = 10,
+//   y = 20;
+// let z = x + y;
+// alert(z);
+//
+// let x = "10",
+//   y = "20";
+// let z = x + y;
+// alert(z);
+//
+// let x = 20,
+//   y = "10";
+// let z = x + y;
+// alert(z);
+//
+// let x = 10,
+//   y = 20;
+// let z = "The result is:" + x + y;
+// alert(z);
+//
+// let x = 10,
+//   y = 20,
+//   z = "30";
+// let result = x + y + z;
+// alert(result);
